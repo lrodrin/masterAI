0f18ab47ab1108a6b3209350516676293b5075b7
lrodrin <lrodrin@gmail.com>
2020-01-14 00:31:31 +0100

    entregable nueva práctica en R

---
+++ A3/Prácticas en R/Unidad 2/script.R
@@ -0,0 +1,15 @@
+#1. Carga los datos en R. Nombra las columnas para identificar major el tablero, 
+#como si se visitarán de izquierda a derecha y de arriba a abajo.
+#Comprueba si hay valores faltantes.
+data <- read.table("tic-tac-toe.data.txt", header=FALSE, sep=",")
+names(data) <- c("top-left-square", "top-middle-square", "top-right-square",
+"middle-left-square", "middle-middle-square", "middle-right-square", "bottom-left-square", 
+"bottom-middle-square", "bottom-right-square", "Class")
+any(is.na(data))
+
+#2. Lee la sección "data splitting" de la web de “caret”. A continuación parte los
+#datos en 70% para entrenamiento y 30% de test manteniendo la proporción original de 
+#clases.
+trainIndex <- createDataPartition(data$Class, p=.7, list=FALSE, times=1)
+dataTrain <- data[ trainIndex,]
+dataTest  <- data[ -trainIndex,]
