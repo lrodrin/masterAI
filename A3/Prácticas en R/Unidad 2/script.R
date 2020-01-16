539b00e79b9dc335ccaa6b2f7766bfc332ff7336
Laura Rodríguez Navas <laurarodrigueznavas@MacBook-Pro-de-Laura.local>
2020-01-16 23:16:51 +0100

    update script R práctica unidad 2

--- A3/Prácticas en R/Unidad 2/script.R
+++ A3/Prácticas en R/Unidad 2/script.R
@@ -1,29 +1,67 @@
 #1. Carga los datos en R. Nombra las columnas para identificar major el tablero, 
 #como si se visitarán de izquierda a derecha y de arriba a abajo.
-#Comprueba si hay valores faltantes.
 data <- read.table("tic-tac-toe.data.txt", header=FALSE, sep=",")
 names(data) <- c("top-left-square", "top-middle-square", "top-right-square",
 "middle-left-square", "middle-middle-square", "middle-right-square", "bottom-left-square", 
 "bottom-middle-square", "bottom-right-square", "Class")
+
+#Comprueba si hay valores faltantes.
 any(is.na(data))
 
 #2. Lee la sección "data splitting" de la web de “caret”. A continuación parte los
 #datos en 70% para entrenamiento y 30% de test manteniendo la proporción original de 
 #clases.
 library(caret)
+set.seed(998)
 inTraining <- createDataPartition(data$Class, p=.7, list=FALSE)
 data_training <- data[ inTraining,]
 data_testing  <- data[-inTraining,]
 
 #3.
-#train_control <- trainControl(method="boot", number=100)
-#model <- train(Class~., data=data_training, trControl=train_control, method="nb")
-#print(model)
+fitControl <- trainControl(method = "cv", 
+                           number = 10, 
+                           repeats = 1)
+# Naive Bayes
 library(naivebayes)
-set.seed(1)
-naive_bayes_via_caret <- train(Class ~ ., 
-                               data = data_training, 
-                               method = "naive_bayes", 
-                               usepoisson = TRUE)
+set.seed(825)
+nb <- train(Class ~ ., data = data_training, 
+            method = "naive_bayes",
+            trControl = fitControl)
 
-naive_bayes_via_caret
+# Decision Tree
+library(rpart)
+set.seed(825)
+dt <- train(Class ~ ., data = data_training, 
+            method = "rpart",
+            trControl = fitControl,
+            tuneLength = 10)
+
+# Neural Network
+library(nnet)
+set.seed(825)
+nn <- train(Class ~ ., data = data_training, 
+            method = "nnet",
+            trControl = fitControl)
+
+# Nearest Neighbour
+
+# SVM (linear kernel)
+library(kernlab)
+set.seed(825)
+svm <- train(Class ~ ., data = data_training, 
+            method = "svmLinear",
+            trControl = fitControl)
+
+resamps <- resamples(list(NB = nb,
+                          DT = dt,
+                          NN = nn,
+                          SVM = svm))
+resamps
+summary(resamps)
+
+
+
+
+
+
+
