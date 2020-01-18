c3241222e836e203f44e064a7b95695b3f8b6fc2
Laura <laura.rodriguez.navas@upc.edu>
2020-01-18 18:54:35 +0100

    update R script

--- A3/Prácticas en R/Unidad 2/script.R
+++ A3/Prácticas en R/Unidad 2/script.R
@@ -1,72 +1,120 @@
-#1. Carga los datos en R. Nombra las columnas para identificar major el tablero, 
-#como si se visitarán de izquierda a derecha y de arriba a abajo.
+# 1.
 data <- read.table("tic-tac-toe.data.txt", header=FALSE, sep=",")
-names(data) <- c("top-left-square", "top-middle-square", "top-right-square",
-"middle-left-square", "middle-middle-square", "middle-right-square", "bottom-left-square", 
-"bottom-middle-square", "bottom-right-square", "Class")
+names(data) <- c("top-left-square", 
+                 "top-middle-square", 
+                 "top-right-square", 
+                 "middle-left-square", 
+                 "middle-middle-square", 
+                 "middle-right-square", 
+                 "bottom-left-square", 
+                 "bottom-middle-square",
+                 "bottom-right-square", 
+                 "Class")
+# make valid Column Names 
+colnames(data) <- make.names(colnames(data))
 
-#Comprueba si hay valores faltantes.
+# check for missing values
 any(is.na(data))
 
-#2. Lee la sección "data splitting" de la web de “caret”. A continuación parte los
-#datos en 70% para entrenamiento y 30% de test manteniendo la proporción original de 
-#clases.
+# 2.
+library(lattice)
+library(ggplot2)
 library(caret)
-set.seed(998)
+set.seed(825)
 inTraining <- createDataPartition(data$Class, p=.7, list=FALSE)
 data_training <- data[ inTraining,]
 data_testing  <- data[-inTraining,]
 
 #3.
-fitControl <- trainControl(method = "cv", 
-                           number = 10, 
-                           repeats = 1)
-# Naive Bayes
+# specifiy the type of resampling
+fitControl <- trainControl(method="repeatedcv", 
+                           number=10, 
+                           repeats=1)
+# Model Naive Bayes
+library(e1071)
 library(naivebayes)
 set.seed(825)
-nb <- train(Class ~ ., data = data_training, 
-            method = "naive_bayes",
-            trControl = fitControl)
+nb <- train(Class ~ ., 
+            data=data_training, 
+            method="naive_bayes",
+            trControl=fitControl)
+nb
 
-# Decision Tree
+# Model Decision Tree
 library(rpart)
 set.seed(825)
-dt <- train(Class ~ ., data = data_training, 
-            method = "rpart",
-            trControl = fitControl,
-            tuneLength = 10)
+dt <- train(Class ~ ., 
+            data=data_training, 
+            method="rpart2",
+            trControl=fitControl)
+dt
 
-# Neural Network
+# Model Neural Network
 library(nnet)
 set.seed(825)
-nn <- train(Class ~ ., data = data_training, 
-            method = "nnet",
-            trControl = fitControl)
+nn <- train(Class ~ ., 
+            data=data_training, 
+            method="nnet",
+            trControl=fitControl)
+nn
 
-# Nearest Neighbour
-library(snn)
+# Model Nearest Neighbour
 set.seed(825)
-snn <- train(Class ~ ., data = data_training, 
-             method = "snn",
-             trControl = fitControl)
+knn <- train(Class ~ ., 
+             data=data_training, 
+             method="knn",
+             trControl=fitControl)
+knn
 
-# SVM (linear kernel)
+# Model SVM (linear kernel)
 library(kernlab)
 set.seed(825)
-svm <- train(Class ~ ., data = data_training, 
-            method = "svmLinear",
-            trControl = fitControl)
+svm <- train(Class ~ ., 
+             data=data_training, 
+             method="svmLinear",
+             trControl=fitControl)
+svm
 
-resamps <- resamples(list(NB = nb,
-                          NN = nn,
-                          KNN = snn,
-                          SVM = svm))
-resamps
+# Collecting the training resampling results
+resamps <- resamples(list("Naive Bayes"=nb,
+                          "Decision Tree"=dt,
+                          "Neural Network"=nn,
+                          "Nearest Neighbour"=knn,
+                          "SVM (linear kernel)" = svm))
 summary(resamps)
 
+#4.
+# Model Naive Bayes
+nbPredict <- predict(nb, newdata=data_testing)
+confusionMatrix(nbPredict, data_testing$Class)
 
+# Model Decision Tree
+dtPredict <- predict(dt, newdata=data_testing)
+confusionMatrix(dtPredict, data_testing$Class)
 
+# Model Neural Network
+nnPredict <- predict(nn, newdata=data_testing)
+confusionMatrix(nnPredict, data_testing$Class)
 
+# Model Nearest Neighbour
+knnPredict <- predict(knn, newdata=data_testing)
+confusionMatrix(knnPredict, data_testing$Class)
 
+# Model SVM (linear kernel)
+svmPredict <- predict(svm, newdata=data_testing)
+confusionMatrix(svmPredict, data_testing$Class)
 
+# Collecting the testing resampling results
+postResample(nbPredict, data_testing$Class)
+postResample(dtPredict, data_testing$Class)
+postResample(nnPredict, data_testing$Class)
+postResample(knnPredict, data_testing$Class)
+postResample(svmPredict, data_testing$Class)
 
+# Calculate AUC value
+library(AUC)
+
+#5.
+#a)
+#b)
+#c)
