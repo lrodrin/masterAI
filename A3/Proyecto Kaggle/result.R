30cecc2c1742eb5c394cf22cfb4502b5bcb61137
lrodrin <lrodrin@gmail.com>
2020-05-31 21:25:39 +0200

    finished Kaggle project

--- A3/Proyecto Kaggle/result.R
+++ A3/Proyecto Kaggle/result.R
@@ -15,18 +15,18 @@ cat("splitting data to train and test......\n")
 train <- data[!is.na(data$shot_made_flag),]
 test <- data[is.na(data$shot_made_flag),]
 
-
 cat("precessing the train data......\n")
 train$shot_made_flag <- as.factor(train$shot_made_flag)
-outliers <- boxplot(train$shot_distance, plot=FALSE)$out
-min(outliers)
-
-#handle with the train features
 train$shot_distance[train$shot_distance > 40] <- 40
 train$time_remaining <- train$minutes_remaining * 60 + train$seconds_remaining
 train <- train[ , !(names(train) %in% drops)]
 
-#normalize function
+cat("precessing the test data......\n")
+test$shot_distance[test$shot_distance > 40] <- 40
+test$time_remaining <- test$minutes_remaining * 60 + test$seconds_remaining
+test <- test[ , !(names(test) %in% drops)]
+
+#normalize train data
 myNormalize <- function (target) {
   (target - min(target))/(max(target) - min(target))
 }
@@ -35,11 +35,7 @@ train$time_remaining <- myNormalize(train$time_remaining)
 train_dat <- data.frame(train$shot_distance, train$time_remaining, train$shot_made_flag)
 colnames(train_dat) <- c("shot_distance", "time_remaining", "shot_made_flag")
 
-#handle with the test features
-test$shot_distance[test$shot_distance > 40] <- 40
-test$time_remaining <- test$minutes_remaining * 60 + test$seconds_remaining
-test <- test[ , !(names(test) %in% drops)]
-
+#normalize test data
 test$shot_distance <- myNormalize(test$shot_distance)
 test$time_remaining <- myNormalize(test$time_remaining)
 test_dat <- data.frame(test$shot_distance, test$time_remaining, test$shot_made_flag)
@@ -57,7 +53,7 @@ newdf <- data.frame(shot_id=train$shot_id, shot_made_flag=pred)
 newdf$shot_made_flag <- myNormalize(newdf$shot_made_flag)
 preds_th <- ifelse(as.numeric(pred) > 0.5,1,0)
 
-# make the Confusion Matrix
+# make the confusion matrix
 cm <- table(newdf$shot_made_flag, preds_th)
 accuracy <- (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[2,2] + cm[1,2] + cm[2,1])
 accuracy
@@ -68,11 +64,6 @@ newdata <- data.frame(test_dat[,-3])
 pred <- predict(model, newdata, type = "response")
 submission <- data.frame(shot_id=test$shot_id, shot_made_flag=pred)
 submission$shot_made_flag <- myNormalize(submission$shot_made_flag)
-
-# make the Confusion Matrix
-cm <- table(newdf$shot_made_flag, preds_th)
-accuracy <- (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[2,2] + cm[1,2] + cm[2,1])
-error <- (1-accuracy) * 100
 
 cat("saving the submission file\n");
 write.csv(submission, "glm.csv", row.names = FALSE)
