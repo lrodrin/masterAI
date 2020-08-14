aa0e67629eff9dbf9983f410a23343ebb1b93d88
Laura Rodríguez Navas <lrodrin@users.noreply.github.com>
2020-08-14 19:27:37 +0200

    Update models.R

--- A7/Flujo de análisis en clasificación supervisada R-caret/models.R
+++ A7/Flujo de análisis en clasificación supervisada R-caret/models.R
@@ -1,39 +1,115 @@
-# PARALELIZACIÓN DE PROCESO
-#===============================================================================
+library(caret)
 library(doParallel)
-cl <- makePSOCKcluster(4, setup_strategy = "sequential") # number of cores to use
-registerDoParallel(cl)
 
-# HIPERPARÁMETROS, NÚMERO DE REPETICIONES Y SEMILLAS PARA CADA REPETICIÓN
-#===============================================================================
-particiones  <- 10
-repeticiones <- 5
-
-# Hiperparámetros
-hiperparametros <- data.frame(k = c(1, 2, 3))
+partitions <- 5
+reteats <- 3
 
 set.seed(123)
-seeds <- vector(mode = "list", length = (particiones * repeticiones) + 1)
-for (i in 1:(particiones * repeticiones)) {
-  seeds[[i]] <- sample.int(1000, nrow(hiperparametros)) 
-}
-seeds[[(particiones * repeticiones) + 1]] <- sample.int(1000, 1)
+cv.folds <- createMultiFolds(train.df$target, k=5, times=3)
+cv.cntrl <- trainControl(method = "repeatecv", number = 5, repeats = 3,
+                         index = cv.folds, allowParallel = TRUE, returnResamp="final",
+                         verboseIter=FALSE)
+gc()
 
-# DEFINICIÓN DEL ENTRENAMIENTO
-#===============================================================================
-control_train <- trainControl(method = "repeatedcv", number = particiones,
-                              repeats = repeticiones, seeds = seeds,
-                              returnResamp = "final", verboseIter = FALSE,
-                              allowParallel = TRUE)
+# GLMNET
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
 
-# AJUSTE DEL MODELO
-# ==============================================================================
-set.seed(342)
-modelo_knn <- train(target ~ ., data = train.df,
-                    method = "knn",
-                    tuneGrid = hiperparametros,
-                    metric = "Accuracy",
-                    trControl = control_train)
-modelo_knn
-
+model_glmnet <- train(target ~ ., data=train.df,
+                      method="glmnet",
+                      metric="Accuracy",
+                      trControl=cv.cntrl,
+                      tuneLenght=15)
+model_glmnet
 stopCluster(cl)
+
+# RF
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
+
+model_rf <- train(target ~ ., data=train.df,
+                      method="rf",
+                      metric="Accuracy",
+                      trControl=cv.cntrl,
+                      tuneGrid=expand.grid(mtry=c(3, 4, 5, 7)))
+model_rf
+stopCluster(cl)
+
+# GBM
+library(gbm)
+
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
+
+cv.grid <- expand.grid(interaction.depth=c(1, 2),
+                       n.trees=100,
+                       shrinkage=c(0.001, 0.01, 0.1),
+                       n.minobsinnode=c(2, 5, 25))
+
+model_gbm <- train(target ~ ., data=train.df,
+                  method="gbm",
+                  metric="Accuracy",
+                  trControl=cv.cntrl,
+                  tuneGrid=cv.grid,
+                  distribution="adaboost",
+                  verbose=FALSE)
+model_gbm
+stopCluster(cl)
+
+# KNN
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
+
+cv.grid <- expand.grid(k=1:3)
+
+model_knn <- train(target ~ ., data=train.df,
+                   method="knn",
+                   metric="Accuracy",
+                   trControl=cv.cntrl,
+                   tuneGrid=cv.grid)
+model_knn
+stopCluster(cl)
+
+# NB
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
+
+model_nb <- train(target ~ ., data=train.df,
+                      method="nb",
+                      metric="Accuracy",
+                      trControl=cv.cntrl,
+                      tuneLenght=15)
+model_nb
+stopCluster(cl)
+
+# SVM
+cl <- makePSOCKcluster(4, setup_strategy="sequential")
+registerDoParallel(cl)
+
+cv.grid <- expand.grid(sigma=c(0.001, 0.01, 0.1, 0.5, 1),
+                       C=c(1, 20, 50, 100))
+
+model_svm <- train(target ~ ., data=train.df,
+                      method="svmRadial",
+                      metric="Accuracy",
+                      trControl=cv.cntrl,
+                      tuneLenght=15)
+model_svm
+stopCluster(cl)
+
+#
+
+models <- list(KNN=model_knn, NB=model_nb, logistic=model_glmnet,
+               RF=model_rf, boosting=model_gbm, SVMRadial=model_svm)
+
+resamps <- resamples(models)
+summary(resamps)
+
+# predict on test with SVMRadial
+pred_svm <- predict(model_svm, test.df, type="raw")
+
+# submission
+submission <- read.csv("sample_submission.csv")
+submission$target <- ifelse(pred_svm=="No", 0, 1)
+head(submission)
+write.csv(submission, "submission.csv", row.names=FALSE)
