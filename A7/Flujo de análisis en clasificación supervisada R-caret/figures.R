7c4fb307342204430cefdd184616f328943b6c48
lrodrin <lrodrin@gmail.com>
2020-08-18 01:06:19 +0200

    update document trabajo metodos supervisados

--- A7/Flujo de análisis en clasificación supervisada R-caret/figures.R
+++ A7/Flujo de análisis en clasificación supervisada R-caret/figures.R
@@ -10,7 +10,7 @@ emocion.df$target <- as.numeric(emocion.df$target)
 
 cor(emocion.df) %>% 
   ggcorrplot(lab = TRUE, 
-             title = "Correlation matrix between keyword sentiments and target",
+             title = "Matriz de correlación entre los \nsentimientos de keyword y target",
              legend.title = "correlation")
 
 stopCluster(cl)
@@ -21,11 +21,25 @@ location.freq[which(location.freq>10)]
 barplot(location.freq[which(location.freq>10)], las = 2,  
         main = "Frequency of Location")
 
+ggplot(complete_df[complete.cases(complete_df), ], aes(x = target, y = TextLength, fill = target)) +
+  theme_bw() +
+  geom_boxplot() +
+  labs(y = "longitud del texto",
+       title = "Distribución de longitudes text con target")
 
-train %>% select(text) %>% unique() %>% head(10)
-count(train %>% select(text) %>% unique())
 
-text.freq <- table(unlist(train %>% select(text)))
-text.freq[which(text.freq > 10)]
-barplot(text.freq[which(text.freq>10)], las = 2,  
-        ylab = "Frequency")
+porqué primero intentaremos averiguar la longitud del texto de los tweets agregando una nueva característica (TextLength).
+
+```{r}
+complete_df$TextLength <- nchar(complete_df$text)
+summary(complete_df$TextLength)
+```
+Existe una gran variación en la longitud de los textos tweets. La longitud mínima es 5 y la longitud máxima es 169. Veamos si los tweets más cortos están hablando de un desastre real o no. Usando **complete_df[complete.cases(complete_df), ]** omitiremos los 3263 valores perdidos de la variable **target**.
+
+```{r fig.align='center', out.width='70%'}
+ggplot(complete_df[complete.cases(complete_df), ], aes(x = target, y = TextLength, fill = target)) +
+  theme_bw() +
+  geom_boxplot() +
+  labs(y = "longitud del texto",
+       title = "Distribución de longitudes text con target")
+```
