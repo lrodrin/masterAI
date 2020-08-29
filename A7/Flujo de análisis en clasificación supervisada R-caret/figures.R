f6269d752281a97a77d7ab4a770e8b4999d4b496
lrodrin <lrodrin@gmail.com>
2020-08-29 17:05:09 +0200

    update new submission to Kaggle. finished prediction section

--- A7/Flujo de análisis en clasificación supervisada R-caret/figures.R
+++ A7/Flujo de análisis en clasificación supervisada R-caret/figures.R
@@ -31,20 +31,3 @@ ggplot(freq_train_df, aes(reorder(term, freq), freq)) + theme_bw() +
   geom_bar(stat = "identity")  + 
   coord_flip() + 
   theme(axis.text.y = element_text(size=7))
-
-
-porqué primero intentaremos averiguar la longitud del texto de los tweets agregando una nueva característica (TextLength).
-
-```{r}
-complete_df$TextLength <- nchar(complete_df$text)
-summary(complete_df$TextLength)
-```
-Existe una gran variación en la longitud de los textos tweets. La longitud mínima es 5 y la longitud máxima es 169. Veamos si los tweets más cortos están hablando de un desastre real o no. Usando **complete_df[complete.cases(complete_df), ]** omitiremos los 3263 valores perdidos de la variable **target**.
-
-```{r fig.align='center', out.width='70%'}
-ggplot(complete_df[complete.cases(complete_df), ], aes(x = target, y = TextLength, fill = target)) +
-  theme_bw() +
-  geom_boxplot() +
-  labs(y = "longitud del texto",
-       title = "Distribución de longitudes text con target")
-```
