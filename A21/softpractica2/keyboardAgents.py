a475f94a259ccf2a188b500e04ddc9ed27867b57
lrodrin <lrodrin@gmail.com>
2020-10-20 23:07:02 +0200

    global refactor de softpractica2

--- A21/softpractica2/keyboardAgents.py
+++ A21/softpractica2/keyboardAgents.py
@@ -16,26 +16,25 @@ from game import Directions
 from game import GameStateData
 import random
 
-
 ##Isa
 import sys
 
 
 class KeyboardAgent(Agent):
-   # NOTE: Arrow keys also work.
-    WEST_KEY  = 'a'
-    EAST_KEY  = 'd'
+    # NOTE: Arrow keys also work.
+    WEST_KEY = 'a'
+    EAST_KEY = 'd'
     NORTH_KEY = 'w'
     SOUTH_KEY = 's'
     STOP_KEY = 'q'
 
-    def __init__( self, index = 0 ):
+    def __init__(self, index=0):
 
         self.lastMove = Directions.STOP
         self.index = index
         self.keys = []
 
-    def getAction( self, state):
+    def getAction(self, state):
         from graphicsUtils import keys_waiting
         from graphicsUtils import keys_pressed
         keys = keys_waiting() + keys_pressed()
@@ -60,10 +59,8 @@ class KeyboardAgent(Agent):
 
     def getMove(self, legal):
         move = Directions.STOP
-        if   (self.WEST_KEY in self.keys or 'Left' in self.keys) and Directions.WEST in legal:  move = Directions.WEST
-        if   (self.EAST_KEY in self.keys or 'Right' in self.keys) and Directions.EAST in legal: move = Directions.EAST
-        if   (self.NORTH_KEY in self.keys or 'Up' in self.keys) and Directions.NORTH in legal:   move = Directions.NORTH
-        if   (self.SOUTH_KEY in self.keys or 'Down' in self.keys) and Directions.SOUTH in legal: move = Directions.SOUTH
-        return move        
-
-        
+        if (self.WEST_KEY in self.keys or 'Left' in self.keys) and Directions.WEST in legal:  move = Directions.WEST
+        if (self.EAST_KEY in self.keys or 'Right' in self.keys) and Directions.EAST in legal: move = Directions.EAST
+        if (self.NORTH_KEY in self.keys or 'Up' in self.keys) and Directions.NORTH in legal:   move = Directions.NORTH
+        if (self.SOUTH_KEY in self.keys or 'Down' in self.keys) and Directions.SOUTH in legal: move = Directions.SOUTH
+        return move
