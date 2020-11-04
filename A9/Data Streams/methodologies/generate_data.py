3169e8c65074630a4fdee17230a6934a112c161b
lrodrin <lrodrin@gmail.com>
2020-11-04 19:18:31 +0100

    upload data generator

--- A9/Data Streams/methodologies/generate_data.py
+++ A9/Data Streams/methodologies/generate_data.py
@@ -5,8 +5,15 @@ x = int(input("Enter x value (1 <= x <= N): "))
 
 if isinstance(N, int) and isinstance(x, int):  # check if N and x are integers
     if 1 <= x <= N:  # check if 1 <= x <= N
-        permutations = random.sample(range(1, N+1), N)  # generate permutations from 1 to N
-        permutations.remove(x)  # remove x from permutations
-        print(permutations)
+        try:
+            permutations = random.sample(range(1, N + 1), N)  # generate permutations from 1 to N
+            permutations.remove(x)  # remove x from permutations
+
+            # write permutations to TXT file
+            with open('data.txt', 'w') as outfile:
+                outfile.write("\n".join(str(item) for item in permutations))
+
+        except Exception as e:
+            raise Exception("Cannot generate the input file, {}".format(e))
 else:
     raise Exception("N and x must be integer values")
