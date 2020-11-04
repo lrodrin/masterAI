0eb12ce01ceb46484c42886d32f0db1a945a791c
lrodrin <lrodrin@gmail.com>
2020-11-04 20:36:27 +0100

    upload find number code for data streams evaluation

---
+++ A9/Data Streams/methodologies/find_number.py
@@ -0,0 +1,19 @@
+import random
+
+from generate_data import createInputFile
+
+N = 5000
+# unordered sequence P from 1 to N
+P = [n for n in random.sample(range(1, N + 1), N)]
+
+x = 221
+# create input TXT file without x
+createInputFile(N, x)
+
+# find missing x in Q
+Q = open('data.txt', 'r').read().splitlines()
+for n in Q:  # for each number inside Q sequence
+    if int(n) in P:  # if number is in P
+        P.remove(int(n))  # n is removed from P
+
+print("Missing number: {}".format(P[0]))
--- A9/Data Streams/methodologies/generate_data.py
+++ A9/Data Streams/methodologies/generate_data.py
@@ -1,19 +1,28 @@
 import random
 
-N = int(input("Enter N value: "))
-x = int(input("Enter x value (1 <= x <= N): "))
 
-if isinstance(N, int) and isinstance(x, int):  # check if N and x are integers
-    if 1 <= x <= N:  # check if 1 <= x <= N
-        try:
-            permutations = random.sample(range(1, N + 1), N)  # generate permutations from 1 to N
-            permutations.remove(x)  # remove x from permutations
+def createInputFile(N, x):
+    """
+    Create TXT input file
 
-            # write permutations to TXT file
-            with open('data.txt', 'w') as outfile:
-                outfile.write("\n".join(str(item) for item in permutations))
+    :param N: total number of elements in a sequence
+    :param x: element to delete in a sequence
+    :type N: int
+    :type x: int
+    """
+    if isinstance(N, int) and isinstance(x, int):  # check if N and x are integers
+        if 1 <= x <= N:  # check if 1 <= x <= N
+            try:
+                permutations = random.sample(range(1, N + 1), N)  # generate permutations from 1 to N
+                permutations.remove(x)  # remove x from permutations
 
-        except Exception as e:
-            raise Exception("Cannot generate the input file, {}".format(e))
-else:
-    raise Exception("N and x must be integer values")
+                # write permutations to TXT file
+                with open('data.txt', 'w') as outfile:
+                    outfile.write("\n".join(str(item) for item in permutations))
+
+                outfile.close()
+
+            except Exception as e:
+                raise Exception("Cannot generate the input file, {}".format(e))
+    else:
+        raise Exception("N and x must be integer values")
