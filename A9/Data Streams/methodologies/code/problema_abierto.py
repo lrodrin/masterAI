77174947a3e8622b5749c0a37ed46f1cb308907d
lrodrin <lrodrin@gmail.com>
2020-11-07 21:10:47 +0100

    resolved problema abierto - find missing number from data stream

--- A9/Data Streams/methodologies/code/problema_abierto.py
+++ A9/Data Streams/methodologies/code/problema_abierto.py
@@ -1,10 +1,13 @@
-x = 9  # elemento a eliminar en Q
-P = [10, 18, 11, 3, 6, 15, 7, 14, 2, 8, 1, 9, 4, 16, 12, 5, 17, 13]  # lista desordenada del 1 al 18
-Q = P.copy()
-Q.remove(9)  # permutación de P a la que le falta x
+# La longitud de la lista es N-1. Entonces, la suma de todos los N elementos, es decir, la suma de los números de 1 a N
+# se puede calcular usando la fórmula n * (n + 1) / 2. Encontramos la suma de todos los elementos de la lista y la
+# restamos de la suma de los primeros N números naturales, el resultado de la resta será el valor del elemento faltante.
 
-for n in Q:  # para cada elemento de Q
-    if n in P:  # si está en P, lo borramos
-        P.remove(n)
+def findMissingNumber(Q):
+    N = len(Q)
+    sum_total = (N + 1) * (N + 2) // 2
+    sum_de_Q = sum(Q)
+    return sum_total - sum_de_Q
 
-print("Missing number: {}".format(P[0]))
+
+Q = [10, 18, 11, 3, 6, 15, 7, 14, 2, 8, 1, 4, 16, 12, 5, 17, 13]
+print(findMissingNumber(Q))
