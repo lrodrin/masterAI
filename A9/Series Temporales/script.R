0e5d64152f85c0fa957c54584d64aa81488065a8
lrodrin <lrodrin@gmail.com>
2020-11-15 20:24:58 +0100

    started trabajo de investigación - series temporales

--- A9/Series Temporales/script.R
+++ A9/Series Temporales/script.R
@@ -1,4 +1,22 @@
+
+
+data <- read.csv("Demanda_2015.csv", header = FALSE, sep = ",")
+
+colnames(data) <- c("date", "time", "demand")
+
+data$date <- as.Date(data$date, format = "%d/%m/%Y")
+
+data["datetime"] <- paste(data$date, data$time)
+data$datetime <- as.POSIXct(data$datetime, tz = "", "%Y-%m-%d %H:%M")
+
+data$date <- NULL
+data$time <- NULL
+
+data <- data[data$datetime >= '2015-01-01 00:00:00' & data$datetime <= '2015-12-31 23:50:00', ]
+
 library(ggplot2)
 
-gas = scan("Demanda_2015.txt")
-plot(gas)
+ggplot(data = data, aes(x = datetime, y = demand)) +
+  geom_line(color = "#00AFBB", size = 0.5) + 
+  ggtitle('Consumo eléctrico, enero-diciembre 2015') +
+  ylab('Consumption in MW')
