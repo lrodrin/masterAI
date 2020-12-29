d819327b06273a1eaffd581ccda0f0a9b915e2a1
lrodrin <lrodrin@gmail.com>
2020-12-29 11:12:06 +0100

    fixed errata Bid Data to Big Data

--- A10/ejemplo_sbt/src/main/scala/uimp/Job2.scala
+++ A10/ejemplo_sbt/src/main/scala/uimp/Job2.scala
@@ -3,7 +3,7 @@ import  uimp.student.{Subject, Student}
 
 object Job2 {
     def main(args: Array[String]) = {
-        val bd = Subject("Bid Data")
+        val bd = Subject("Big Data")
         val student = new Student("Pepe Gotera")
         student.study(bd)
     }
