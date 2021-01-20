91eae1b4a37067c2f921b0c7099d051031b477ad
lrodrin <lrodrin@gmail.com>
2021-01-20 13:54:39 +0100

    update ETL preprocesado code

--- A10/programación batch en Spark/etl.py
+++ A10/programación batch en Spark/etl.py
@@ -1,7 +1,7 @@
-import sys, os, bz2, json, sys
+import sys, os, bz2, json
 
 # lee todos los archivos de un directorio y los descomprime del formato bz2.
-path = "./files/30/01/"
+path = "./30/01/"
 for(dirpath, dirnames, files)in os.walk(path):
     for filename in files:
         filepath = os.path.join(dirpath, filename)
@@ -12,10 +12,10 @@ for(dirpath, dirnames, files)in os.walk(path):
         newfilepath = filepath[:-4] # assuming the filepath ends with .bz2
         open(newfilepath, 'wb').write(data) # write a uncompressed file
         
-with open('./files/sample.json','r') as f:
+with open(path + '00.json','r') as f:
     line = f.readline()
     while line:
         j = json.loads(line)
         # print j # si imprimimos la variable vemos su estructura.
-        print j["user"]["screen_name"].encode("utf-8") # para referirnos a un usuario usaremos su Screen Name.
+        print(j["user"]["screen_name"].encode("utf-8")) # para referirnos a un usuario usaremos su Screen Name.
         line = f.readline()
