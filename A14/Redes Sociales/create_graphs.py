05d08efdba7cd0a9e74d564d0c5a70a76b2c0703
lrodrin <lrodrin@gmail.com>
2021-01-26 21:26:01 +0100

    update gephi project with code to create diseases graphs

--- A14/Redes Sociales/create_graphs.py
+++ A14/Redes Sociales/create_graphs.py
@@ -2,11 +2,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("dataset/diseasome.csv", sep=",")
-df_disease = df[df['0'].str.contains("disease")]
-df_disease = df_disease[df_disease["degree"] > 15]
-print df_disease
-plt.figure(num=None, figsize=(20, 18), dpi=80, facecolor='w', edgecolor='r')
-g = sns.barplot(x="Label", y="degree", data=df_disease)
-plt.xticks(rotation=90)
-plt.savefig('images/plt_degree.png')
+
+def createGraph(values, col):
+    plt.figure(num=None, figsize=(20, 18), dpi=80, facecolor='w', edgecolor='r')
+    g = sns.barplot(x="Label", y=col, data=values)
+    plt.xticks(rotation=90)
+    plt.savefig('images/{}.png'.format(col))
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("dataset/diseasome.csv", sep=",")
+    df_disease = df[df['0'].str.contains("disease")]
+    df_disease = df_disease[df_disease["degree"] > 15]
+
+    colNames = ["degree", "betweenesscentrality", "closnesscentrality", "eigencentrality"]
+    for colName in colNames:
+        createGraph(df_disease, colName)
