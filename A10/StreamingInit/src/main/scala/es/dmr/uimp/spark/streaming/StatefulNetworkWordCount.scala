909c0ba0f844c863598421759c9910cc43a779ef
lrodrin <lrodrin@gmail.com>
2021-02-17 14:56:49 +0100

    conteo total de palabras

--- A10/StreamingInit/src/main/scala/es/dmr/uimp/spark/streaming/StatefulNetworkWordCount.scala
+++ A10/StreamingInit/src/main/scala/es/dmr/uimp/spark/streaming/StatefulNetworkWordCount.scala
@@ -1,5 +1,8 @@
 package es.dmr.uimp.spark.streaming
 
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, State, StateSpec, StreamingContext}
+
 /**
  * Cuenta las palabras recibidas por red.
  *
@@ -17,6 +20,48 @@ object StatefulNetworkWordCount {
 
     StreamingExamples.setStreamingLogLevels()
 
-    // Introduzca su codigo aqui
+    // Establezca la conexión con Spark y el entorno de Streaming
+    val sparkConf = new SparkConf().setAppName("StatefulNetworkWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
+
+    // Configure el nivel de log
+    ssc.sparkContext.setLogLevel("WARN")
+
+    // En este ejercicio, la pipeline va a mantener una cuenta total del número de veces que un término ha aparecido
+    // en el stream. Para que la computación sea fiable, es necesario que periódicamente se haga un backup del estado
+    // del pipeline. Establecemos la localización donde este backup se va a mantener
+    ssc.checkpoint("./checkpoint")
+
+    // Creamos una conexión al socket y distribuimos las palabras encontradas
+    val lines = ssc.socketTextStream(args(0), args(1).toInt)
+    val words = lines.flatMap(_.split(" "))
+    val wordsDstream = words.map(x => (x, 1))
+
+    // Construimos la actualización del conteo total. En paraello, es necesario una función que toma como entrada
+    // la clave, el valor y el estado actual para esa clave, y lleva a cabo la actualización. La salida es el
+    // nuevo valor para el estado
+    val mappingFunc = (word: String, one: Option[Int], state:
+    State[Int]) => {
+      val sum = one.getOrElse(0) + state.getOption.getOrElse(0)
+      val output = (word, sum)
+      state.update(sum)
+      output
+    }
+
+    // Conectamos la actualización del valor con el origen de datos
+    // Comenzamos con un estado inicial
+    val initialRDD = ssc.sparkContext.parallelize(List(("hello", 1), ("world", 1)))
+
+    // Conectamos el calculo con el stream de entrada
+    val stateDstream = wordsDstream.mapWithState(
+      StateSpec.function(mappingFunc).initialState(initialRDD)
+    )
+
+    // Mostramos el nuevo estado por pantalla
+    stateDstream.print()
+
+    // Arrancamos la pipeline
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
