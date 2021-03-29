6ee7fcd182616ca7e871c32483f99355d4fa2f0b
lrodrin <lrodrin@gmail.com>
2021-03-29 18:47:38 +0200

    update preprocess code adding data visualization - práctica bloque II

--- A14/Recuperación de información y minería de texto/kmeans/preprocess.py
+++ A14/Recuperación de información y minería de texto/kmeans/preprocess.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (16, 9)
+plt.style.use('ggplot')
 
 
 def printNcategories():
@@ -26,11 +30,19 @@ df = df[~df['category'].isin(uniqueCategories)]
 printNcategories()
 
 # convert categories to numbers and save it to new column topic
-df['category'] = pd.Categorical(df['category'])  # change the type of the column
-df['topic'] = df['category'].cat.codes
+df['category'] = pd.Categorical(df['category'])
+df['title'] = pd.Categorical(df['title'])
+df['category_c'] = df['category'].cat.codes
+df['title_c'] = df['title'].cat.codes
 
 # re oder df columns
-df = df[['topic', 'category', 'title', 'description']]
+df = df[['category_c', 'category', 'title_c', 'title', 'description']]
 
 # save df to csv
 df.to_csv("books.csv", index=False, sep=",")
+
+# df visualization
+df['category'].value_counts().plot(kind="bar")
+plt.suptitle('Books categories', fontsize=20)
+plt.ylabel('Frequency', fontsize=16)
+plt.show()
