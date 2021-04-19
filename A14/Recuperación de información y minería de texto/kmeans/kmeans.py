5221fffa504b6d8ac8c05d6efc97b6e0e2a0782b
lrodrin <lrodrin@gmail.com>
2021-04-19 11:55:40 +0200

    refactoring latex document sections 3.1 and 3.2 práctica bloque II

--- A14/Recuperación de información y minería de texto/kmeans/kmeans.py
+++ A14/Recuperación de información y minería de texto/kmeans/kmeans.py
@@ -29,10 +29,8 @@ def tokenize_and_stem(text):
     return stems
 
 def tokenize_only(text):
-    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
     tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
     filtered_tokens = []
-    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
     for token in tokens:
         if re.search('[a-zA-Z]', token):
             filtered_tokens.append(token)
