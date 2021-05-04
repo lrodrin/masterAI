ae3d14fc29b56ebfc887483023b944ababfaa21b
lrodrin <lrodrin@gmail.com>
2021-05-05 01:13:59 +0200

    finished pearson documentation and refactor code - práctica SR

--- A13/Recomendación para grupos/recommendation_system.py
+++ A13/Recomendación para grupos/recommendation_system.py
@@ -21,7 +21,7 @@ def print_df(dataframe, nrows):
 # Create movies and ratings dataframes
 movies_df = pd.read_csv('dataset/movies.csv')
 ratings_df = pd.read_csv('dataset/ratings.csv')
-print("[initial movies dataframe]")
+print('[initial movies dataframe]')
 print_df(movies_df, 10)
 
 # Take the year out of the title column and save it in a new column
@@ -31,12 +31,12 @@ movies_df['title'] = movies_df.title.str.replace(regular_expression, '', regex=T
 movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
 # Remove genres column and save movies_df
 movies_df = movies_df.drop('genres', 1)
-print("[final movies dataframe]")
+print('[final movies dataframe]')
 print_df(movies_df, 10)
 
 # Remove timestamp column and save ratings_df
 ratings_df = ratings_df.drop('timestamp', 1)
-print("[final ratings dataframe]")
+print('[final ratings dataframe]')
 print_df(ratings_df, 10)
 
 # Create user dataframe
@@ -47,7 +47,7 @@ user_df = pd.read_csv('dataset/new_user.csv')
 titles = movies_df[movies_df['title'].isin(user_df['title'].tolist())]
 user_df = pd.merge(titles, user_df)
 user_df = user_df.drop('year', 1)  # Remove year column
-print("[new user dataframe]")
+print('[new user dataframe]')
 print_df(user_df, 10)
 
 # Users who have seen the same movies
@@ -55,51 +55,51 @@ movies = ratings_df[ratings_df['movieId'].isin(user_df['movieId'].tolist())]
 users = movies.groupby(['userId'])    # Grouping users by userId
 
 # User 525
-print("[user 525]")
-print_df(users.get_group(525), "all")
+print('[user 525]')
+print_df(users.get_group(525), 'all')
 
 # Users who have more movies in common have more priority
 common_users = sorted(users, key=lambda x: len(x[1]), reverse=True)
 
+# Neighborhood selection
+neighborhood = common_users[0:100]  # Choosing 100 users to do the iterations
+
 # Pearson
-common_users = common_users[0:100]  # Choosing a subset of users to do the iterations.
 pearsonCorrelationDict = {}
-for name, group in common_users:  # For each user
-    # Sorting the current user in such a way that the values don't get mixed up later
+for id, group in neighborhood:
+    # The current user and the new user are ordered in the same way
     user = group.sort_values(by='movieId')
     movies = user_df.sort_values(by='movieId')
 
-    # Get the number of elements (N) for the formula
+    # Number of ratings for user
     nRatings = len(user)
 
-    # Set ratings for movies in common in a list
+    # Common ratings of the current user with the new user
     temp_df = movies[movies['movieId'].isin(user['movieId'].tolist())]
     tempRatingList = temp_df['rating'].tolist()
 
-    # Set user ratings in a list
-    tempGroupList = user['rating'].tolist()
+    # Ratings of the current user
+    tempUserList = user['rating'].tolist()
 
-    # Calculate the Pearson Correlation between two users, x and y
+    # Calculate the Pearson Correlation between the current user and new user
     Uxx = sum([i ** 2 for i in tempRatingList]) - pow(sum(tempRatingList), 2) / float(nRatings)
-    Uyy = sum([i ** 2 for i in tempGroupList]) - pow(sum(tempGroupList), 2) / float(nRatings)
-    Uxy = sum(i * j for i, j in zip(tempRatingList, tempGroupList)) - sum(tempRatingList) * sum(tempGroupList) / float(
-        nRatings)
+    Uyy = sum([i ** 2 for i in tempUserList]) - pow(sum(tempUserList), 2) / float(nRatings)
+    Uxy = sum(i * j for i, j in zip(tempRatingList, tempUserList)) - sum(tempRatingList) * sum(tempUserList) / float(nRatings)
 
     # If the denominator is nonzero, then we divide, otherwise the correlation is 0
     if Uxx != 0 and Uyy != 0:
-        pearsonCorrelationDict[name] = Uxy / sqrt(Uxx * Uyy)
-
+        pearsonCorrelationDict[id] = Uxy / sqrt(Uxx * Uyy)
     else:
-        pearsonCorrelationDict[name] = 0
+        pearsonCorrelationDict[id] = 0
 
-# print(pearsonCorrelationDict.items())
+print(pearsonCorrelationDict.items())
 
 # Create pearson dataframe
 pearson_df = pd.DataFrame.from_dict(pearsonCorrelationDict, orient='index')
 pearson_df.columns = ['similarityIndex']
 pearson_df['userId'] = pearson_df.index
 pearson_df.index = range(len(pearson_df))
-#print_df(pearson_df)
+print_df(pearson_df, 10)
 
 # Get the top 50 most similar users
 topUsers = pearson_df.sort_values(by='similarityIndex', ascending=False)[0:50]
@@ -128,5 +128,5 @@ recommendation_df = movies_df.loc[movies_df['movieId'].isin(recommendation_df.he
 #print_df(recommendation_df)
 
 # Save recommendations to CSV file
-recommendation_df[['title', 'year']].to_csv("recommendation.csv", index=False)
+recommendation_df[['title', 'year']].to_csv('recommendation.csv', index=False)
 
