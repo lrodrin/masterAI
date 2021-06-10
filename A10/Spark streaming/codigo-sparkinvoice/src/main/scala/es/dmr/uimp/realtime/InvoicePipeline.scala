d8eae38e1d7d9b12dd5144a42d54778631dc52d1
lrodrin <lrodrin@gmail.com>
2021-06-10 22:25:04 +0200

    built the rest of the invoice pipeline detecting anomalies

--- A10/Spark streaming/codigo-sparkinvoice/src/main/scala/es/dmr/uimp/realtime/InvoicePipeline.scala
+++ A10/Spark streaming/codigo-sparkinvoice/src/main/scala/es/dmr/uimp/realtime/InvoicePipeline.scala
@@ -1,6 +1,5 @@
 package es.dmr.uimp.realtime
 
-import org.apache.commons.lang3.StringUtils
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.clustering._
@@ -8,13 +7,17 @@ import org.apache.spark.streaming._
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import java.util.HashMap
 
+import java.util.HashMap
 import com.univocity.parsers.csv.{CsvParser, CsvParserSettings}
 import es.dmr.uimp.clustering.KMeansClusterInvoices
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.util.Saveable
 import org.apache.spark.rdd.RDD
+
+import java.util
+import scala.collection.mutable.ArrayBuffer
 
 object InvoicePipeline {
 
@@ -59,6 +62,14 @@ object InvoicePipeline {
 
     // TODO: rest of pipeline
 
+    val invoices: DStream[(String, Invoice)] = purchasesStream
+      .filter(tuple => !isWrongPurchase(tuple._2))
+      .window(Seconds(40), Seconds(1))
+      .updateStateByKey(updateFunction)
+
+    detectAnomalies(invoices, kMeansModel.value, kMeansThreshold.value, "anomalias_kmeans", broadcastBrokers)
+    detectAnomalies(invoices, bisectionKMeans.value, bisectionThreshold.value, "anomalias_bisect_kmeans", broadcastBrokers)
+
     ssc.start() // Start the computation
     ssc.awaitTermination()
   }
@@ -67,14 +78,14 @@ object InvoicePipeline {
     rdd.foreachPartition(partition => {
       val producer = new KafkaProducer[String, String](kafkaConf(kafkaBrokers.value))
       partition.foreach(record => {
-        producer.send(new ProducerRecord[String, String](topic, record._1, record._2.toString))
+        producer.send(new ProducerRecord[String, String](topic, record._1, record._2))
       })
       producer.close()
     })
   }
 
   def kafkaConf(brokers: String) = {
-    val props = new HashMap[String, Object]()
+    val props = new util.HashMap[String, Object]()
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
@@ -82,7 +93,7 @@ object InvoicePipeline {
   }
 
   /**
-   * Load the KMeans model information: centroid and threshold
+   * Load the KMeans model information: centroid and threshold.
    */
   def loadKMeansAndThreshold(sc: SparkContext, modelFile: String, thresholdFile: String): (KMeansModel, Double) = {
     val kmeans = KMeansModel.load(sc, modelFile)
@@ -94,7 +105,7 @@ object InvoicePipeline {
   }
 
   /**
-   * Load the KMeans bisection model information: centroid and threshold
+   * Load the KMeans bisection model information: centroid and threshold.
    */
   def loadBisectingKMeansAndThreshold(sc: SparkContext, modelFile: String, thresholdFile: String): (BisectingKMeansModel, Double) = {
     val kmeans = BisectingKMeansModel.load(sc, modelFile)
@@ -116,7 +127,7 @@ object InvoicePipeline {
 
   /**
    * Transforms the Kafka Stream into a new stream
-   * with a the value parsed as a Purchase class
+   * with a the value parsed as a Purchase class.
    */
   def getPurchasesStream(purchasesFeed: DStream[(String, String)]): DStream[(String, Purchase)] = {
     val purchasesStream = purchasesFeed.transform { inputRDD =>
@@ -135,8 +146,7 @@ object InvoicePipeline {
 
   /**
    * Given a purchases feed formatted as csv-like strings,
-   * returns those purchases parsed as objects by using a
-   * csv parser
+   * returns those purchases parsed as objects by using a csv parser.
    */
   def parsePurchase(purchase: String): Purchase = {
     val csvParserSettings = new CsvParserSettings()
@@ -158,7 +168,7 @@ object InvoicePipeline {
 
   /**
    * Given a Purchase Stream, detects the wrong purchases (with any missing or wrong field)
-   * and sends the feedback to the facturas_erroneas Kafka topic
+   * and sends the feedback to the facturas_erroneas Kafka topic.
    */
   def detectWrongPurchases(purchasesStream: DStream[(String, Purchase)], broadcastBrokers: Broadcast[String]): Unit = {
     purchasesStream
@@ -172,7 +182,7 @@ object InvoicePipeline {
   }
 
   /**
-   * Checks if the purchase has any missing or wrong property
+   * Checks if the purchase has any missing or wrong property.
    */
   def isWrongPurchase(purchase: Purchase): Boolean = {
     purchase.invoiceNo == null || purchase.invoiceDate == null || purchase.customerID == null ||
@@ -184,7 +194,7 @@ object InvoicePipeline {
   /**
    * Given a Purchase Stream, detects the cancellations (the quantity property is negative)
    * and calculates the number of cancellations for the last 8 minutes.
-   * It gives this feedback to the cancelaciones Kafka topic every second
+   * It gives this feedback to the cancelaciones Kafka topic every second.
    */
   def detectCancellations(purchasesStream: DStream[(String, Purchase)], broadcastBrokers: Broadcast[String]): Unit = {
     purchasesStream
@@ -196,6 +206,68 @@ object InvoicePipeline {
       .foreachRDD { rdd =>
         publishToKafka("cancelaciones")(broadcastBrokers)(rdd)
       }
+  }
+
+  /**
+   * For a given sequence of purchases (owning to the same invoice), it calculates the invoice properties
+   * and returns a proper formatted object for each sequence.
+   */
+  def updateFunction(purchases: Seq[Purchase], state: Option[Invoice]): Option[Invoice] = {
+    val invoiceNo = purchases.head.invoiceNo
+    val unitPrices = purchases.map(purchase => purchase.unitPrice)
+    val average: Double = unitPrices.sum / unitPrices.length
+    val minimum: Double = unitPrices.min
+    val maximum: Double = unitPrices.max
+    val time = purchases.head.invoiceDate.split(" ")(1).split(":")(0).toDouble
+    val numberOfItems = purchases.map(purchase => purchase.quantity).sum
+    val lines = purchases.length
+    val lastUpdated = 0
+    val customer = purchases.head.customerID
+
+    Some(Invoice(invoiceNo, average, minimum, maximum, time, numberOfItems, lines, lastUpdated, customer))
+  }
+
+  /**
+   * For a given sequence of purchases (owning to the same invoice), it calculates the invoice properties
+   * and returns a proper formatted object for each sequence.
+   */
+  def detectAnomalies(invoices: DStream[(String, Invoice)], model: Saveable, threshold: Double, topic: String,
+                      broadcastBrokers: Broadcast[String]): Unit = {
+    invoices
+      .filter { tuple =>
+        val invoice: Invoice = tuple._2
+        isAnomaly(invoice, model, threshold)
+      }
+      .transform { invoicesTupleRDD =>
+        invoicesTupleRDD.map(invoiceTuple => (invoiceTuple._1, invoiceTuple._2.toString))
+      }
+      .foreachRDD { rdd =>
+        publishToKafka(topic)(broadcastBrokers)(rdd)
+      }
+  }
+
+  /**
+   * For the given invoice, model and threshold, it detects if it is an anomaly by using the distToCentroid function.
+   * It predicts the distance to the assigned centroid for the trained model. If the distance is greater than the
+   * given threshold, it is an anomaly.
+   */
+  def isAnomaly(invoice: Invoice, model: Saveable, threshold: Double): Boolean = {
+    val featuresBuffer = ArrayBuffer[Double]()
+
+    featuresBuffer.append(invoice.avgUnitPrice)
+    featuresBuffer.append(invoice.minUnitPrice)
+    featuresBuffer.append(invoice.maxUnitPrice)
+    featuresBuffer.append(invoice.time)
+    featuresBuffer.append(invoice.numberItems)
+
+    val features = Vectors.dense(featuresBuffer.toArray)
+
+    val distance = model match {
+      case model: KMeansModel => KMeansClusterInvoices.distToCentroidFromKMeans(features, model)
+      case model: BisectingKMeansModel => KMeansClusterInvoices.distToCentroidFromBisectingKMeans(features, model)
+    }
+
+    distance.>(threshold)
   }
 
 }
