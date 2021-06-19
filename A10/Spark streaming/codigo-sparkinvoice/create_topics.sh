d653cde7673a3014716038f76fbece662b58dc95
lrodrin <lrodrin@gmail.com>
2021-06-19 14:26:26 +0200

    added purchases topic in script to create kafka topics

--- A10/Spark streaming/codigo-sparkinvoice/create_topics.sh
+++ A10/Spark streaming/codigo-sparkinvoice/create_topics.sh
@@ -4,3 +4,4 @@ sh /opt/Kafka/kafka_2.11-2.3.0/bin/kafka-topics.sh --create --zookeeper localhos
 sh /opt/Kafka/kafka_2.11-2.3.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic facturas_erroneas --replication-factor 1 --partitions 3
 sh /opt/Kafka/kafka_2.11-2.3.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic anomalias_kmeans --replication-factor 1 --partitions 3
 sh /opt/Kafka/kafka_2.11-2.3.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic anomalias_bisect_kmeans --replication-factor 1 --partitions 3
+sh /opt/Kafka/kafka_2.11-2.3.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --topic purchases --replication-factor 1 --partitions 3
